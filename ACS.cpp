3fbf83ff843c861dd21110a1f09c5e3b37fb7d54
Malcolm Groves <macgroves@gmail.com>
2017-04-01 17:37:34 -0400

    Debug ACS, seems to work now

--- ACS.cpp
+++ ACS.cpp
@@ -6,7 +6,7 @@ ACS::ACS(double a, double b, double e, int colonySize, int numIterations, vector
     //this->tau_0 = t0;
     this->q_0 = q0;
     srand(time(NULL));
-    
+    cout << "ACS constructed" << endl;
 }
 
 void ACS::runACS() {
@@ -15,8 +15,11 @@ void ACS::runACS() {
     
     for(int i = 0; i < num_iterations; ++i) {
         make_tours();
+        
         wear_away();
+        
         add_pheromone();
+        
         clear_ants();
     }
     
@@ -48,7 +51,12 @@ void ACS::add_pheromone() {
         else {
             end_city = best_ant.tour[0];
         }
-        pheromones[start_city][end_city] = evap_rate / lookup_dist(start_city, end_city);
+        if(start_city > end_city) {
+            pheromones[start_city][end_city] = evap_rate / lookup_dist(start_city, end_city);
+        }
+        else {
+            pheromones[end_city][start_city] = evap_rate / lookup_dist(start_city, end_city);
+        }
     }
 }
 
@@ -58,6 +66,7 @@ void ACS::wear_away() {
     for(int i = 0; i < colony_size; ++i) {
         //THIS WILL SEGFAULT UNLESS THE ANTS LAST & FIRST START IS INLCUDED
         for(int j = 0; j < num_cities; ++j) {
+            //cout << j << endl;
             int start_city = colony[i].tour[j];
             int end_city;
             if(j < num_cities - 1) {
@@ -66,7 +75,12 @@ void ACS::wear_away() {
             else {
                 end_city = colony[i].tour[0];
             }
-            pheromones[start_city][end_city] = (1 - epsilon) * pheromones[start_city][end_city] + tau_0 * epsilon;
+            if(start_city > end_city) {
+                pheromones[start_city][end_city] = (1 - epsilon) * lookup_pher(start_city, end_city) + tau_0 * epsilon;
+            }
+            else {
+                pheromones[end_city][start_city] = (1 - epsilon) * lookup_pher(start_city, end_city) + tau_0 * epsilon;
+            }
         }
     }
 }
@@ -83,8 +97,10 @@ void ACS::exploitation_step(int ant_index) {
         if(colony[ant_index].unvisited[i] == true) {
             double path_dist = lookup_dist(curr_city, i);
             double path_pher = lookup_pher(curr_city, i);
-            double city_value = path_pher * pow(1 / path_dist, beta);
             
+
+            double city_value = path_pher * pow(1 / path_dist, beta);
+            //cout << city_value << endl;
             // if this city is the new best, update
             if(max_city_value < city_value) {
                 max_city = i;
@@ -94,33 +110,44 @@ void ACS::exploitation_step(int ant_index) {
     }
     
     // update the ant's tour and unvisited vector
+    //cout << max_city << endl;
     colony[ant_index].unvisited[max_city] = false;
+    //cout << "unvisited" << endl;
     colony[ant_index].tour.push_back(max_city);
-    colony[ant_index].length += dists[curr_city][max_city];
+    colony[ant_index].length += lookup_dist(curr_city, max_city);
+    //cout << "Saved" << endl;
 }
 
 void ACS::make_tours() {
     for(int i = 0; i < colony_size; ++i) {
-        
+    
         
         int starting_city = rand() % num_cities;
+    
         colony[i].tour.push_back(starting_city);
-        
+        colony[i].unvisited[starting_city] = false;
+    
         for(int j = 0; j < num_cities - 1; ++j) {
             double step_prob = (double) rand() / RAND_MAX;
             
             if(step_prob < q_0) {
+                //cout << "Exploitation" << endl;
                 exploitation_step(i);
             }
             else {
+                //cout << "Probabilistic" << endl;
                 probabilistic_next_step(i);
             }
+            
         }
-        
+
+        //cout << "mostly done tours" << endl;
         // make ant return to starting city
-        colony[i].tour.push_back(starting_city);
-        colony[i].length += lookup_dist(starting_city, colony[i].tour.back());
         
+    
+        //cout << colony[i].tour.back() << endl;
+        colony[i].length += lookup_dist(starting_city, colony[i].tour.back());
+        colony[i].tour.push_back(starting_city);
         // update best ant as necessary
         if(colony[i].length < best_ant.length) {
             best_ant = colony[i];
